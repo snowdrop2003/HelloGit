4a4f9a85552e2de1048c6343135bcfc7adb0567f
Yukihiro Akada <yukihiroakada@gmail.com>
2020-08-17 12:54:49 +0900

    【Eloquent】(リレーション) Borad追加処理

--- var/www/html/laravelapp/routes/web.php
+++ var/www/html/laravelapp/routes/web.php
@@ -44,6 +44,12 @@ Route::get('/', function () {
     Route::get('person/del', 'PersonController@delete');
     Route::post('person/del', 'PersonController@remove');
 
+
+    Route::get('board', 'BoardController@index');
+    Route::get('board/add', 'BoardController@add');
+    Route::post('board/add', 'BoardController@create');
+    Route::post('board/del', 'BoardController@remove');
+
 //     Route::get('hello', function () {
 //         return view('hello.index') ;
 //     });
