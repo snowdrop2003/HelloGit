1332c6e2aa4c78e5d3ca9bd9b8426dfeb6bac55f
Yukihiro Akada <yukihiroakada@gmail.com>
2020-08-23 19:06:44 +0900

    【phpUnit】データベーステスト

--- var/www/html/laravelapp/tests/Feature/HelloTest.php
+++ var/www/html/laravelapp/tests/Feature/HelloTest.php
@@ -18,19 +18,52 @@ class HelloTest extends TestCase
      */
     public function testHello()
     {
-        $response = $this->get('/');
-        $response->assertStatus(200);
+        //ダミーで利用するデータ
+        factory(User::class)->create([
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.com',
+            'password' => 'ABCABC',
+        ]);
+        factory(User::class, 10)->create();
 
-        $response = $this->get('/hello');
-        $response->assertStatus(302);
+        $this->assertDatabaseHas('users', [
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.com',
+            'password' => 'ABCABC',
+        ]);
 
-        $user = factory(USER::class)->create();
-        $response = $this->actingAs($user)->get('/hello');
-        $response->assertStatus(200);
+        //ダミーで利用するデータ
+        factory(User::class)->create([
+            'name' => 'XXX',
+            'email' => 'YYY@ZZZ.com',
+            'password' => '123',
+        ]);
+        factory(User::class, 10)->create();
 
-        $response = $this->get('/no_route');
-        $response->assertStatus(404);
+        $this->assertDatabaseHas('users', [
+            'name' => 'XXX',
+            'email' => 'YYY@ZZZ.com',
+            'password' => '123',
+        ]);
 
+
+
+        //▼===== ルート情報テスト =====▼
+//        $response = $this->get('/');
+//        $response->assertStatus(200);
+//
+//        $response = $this->get('/hello');
+//        $response->assertStatus(302);
+//
+//        $user = factory(USER::class)->create();
+//        $response = $this->actingAs($user)->get('/hello');
+//        $response->assertStatus(200);
+//
+//        $response = $this->get('/no_route');
+//        $response->assertStatus(404);
+        //▲===== ルート情報テスト =====▲
+
+        //▼===== 変数テスト =====▼
 //        $this->assertTrue(true);
 //
 //        $arr = [];
@@ -41,5 +74,6 @@ class HelloTest extends TestCase
 //
 //        $n = random_int(0, 100);
 //        $this->assertLessThan(100, $n);
+        //▲===== 変数テスト =====▲
     }
 }
